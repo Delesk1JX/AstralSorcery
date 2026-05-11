5ca65aeb367a7fe777eebf0f8ce758de242f2c27
Delesk1JX <gopstop502@gmail.com>
2026-05-11 18:58:58 +0000

    Ещё фиксы миксинов

--- src/main/java/hellfirepvp/astralsorcery/mixin/MixinCooldownTracker.java
+++ src/main/java/hellfirepvp/astralsorcery/mixin/MixinCooldownTracker.java
@@ -10,11 +10,10 @@ package hellfirepvp.astralsorcery.mixin;
 
 import hellfirepvp.astralsorcery.common.event.CooldownSetEvent;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemCooldowns;
 import net.neoforged.neoforge.common.NeoForge;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 /**
  * This class is part of the Astral Sorcery Mod
@@ -23,19 +22,15 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
  * Created by HellFirePvP
  * Date: 01.01.2022 / 09:52
  */
-@Mixin(ItemCooldowns.class)
+@Mixin(Player.class)
 public class MixinCooldownTracker {
 
-    // TODO: Rewrite for Minecraft 1.21.1 ItemCooldowns system
-    // The cooldown system has changed significantly in 1.21.1
-    // This mixin needs to be applied to ServerPlayer instead
-    /*
-    @ModifyVariable(method = "setCooldown", at = @At("HEAD"), ordinal = 0, argsOnly = true)
+    @ModifyArg(method = "addCooldown", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemCooldowns;addCooldown(Lnet/minecraft/world/item/Item;I)V"), index = 1)
     public int fireCooldownEvent(int cooldownTicks) {
-        ItemCooldowns cooldowns = (ItemCooldowns)(Object) this;
-        // TODO: Find a way to get player reference and fire event
-        return cooldownTicks;
+        Player player = (Player)(Object) this;
+        CooldownSetEvent event = new CooldownSetEvent(player, cooldownTicks);
+        NeoForge.EVENT_BUS.post(event);
+        return event.getResultCooldown();
     }
-    */
 
 }
